a888021bf06aa59e4ef096a8350686ba8e247c7a
Natalie <nwiegand1@hwemail.com>
2017-05-13 16:06:42 -0700

    update

--- skill.py
+++ skill.py
@@ -23,32 +23,32 @@ def feeling_good():
 	feeling_bad_resp = 'That sounds tough. What emotions are you feeling right now?'
 	return statement(feeling_bad_resp)
 
-@ask.intent("Intent4")
+@ask.intent("IntentFour")
 def find_emotion():
-	emotion_q = 'How do you know you are feeling this way?'
+	emotion_q = "How do you know you are feeling this way?"
 	return statement(emotion_q)
 
-@ask.intent("Intent5")
+@ask.intent("IntentFive")
 def sympathize():
 	symp = 'Wow, that seems like a lot to deal with and I can imagine very difficult. Could you imagine things being different?'
 	return statement(symp)
 
-@ask.intent("Intent6")
+@ask.intent("IntentSix")
 def goal_set():
-	goal = 'That is great! What is a specific goal you can mek that can imporve your situation?'
+	goal = 'That is great! What is a specific goal you can make that can imporve your situation?'
 	return statement(goal)
 
-@ask.intent("Intent7")
+@ask.intent("IntentSeven")
 def goal_set_enviro():
 	enviro = 'Identifying a goal is the first step. Next is knowing when you have achieved this goal. How do you know when your goals have been met? What will this look like?'
 	return statement(enviro)
 
-@ask.intent("Intent8")
+@ask.intent("IntentEight")
 def achievable():
 	achieve = 'Do you think this is an achievable and realistic goal?'
 	return statement(achieve)
 
-@ask.intent("Intent9")
+@ask.intent("IntentNine")
 def goal_time():
 	time = 'Great! Now that you have a specific, measurable and realistic goal, what is a realistic time frame to complete your goal?'
 	return statement(time)
