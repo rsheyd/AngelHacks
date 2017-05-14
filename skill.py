f30ab2a205d6364e000acfcda0c4d45ade6763c1
Roman Sheydvasser <s.roman@gmail.com>
2017-05-13 19:15:48 -0700

    updated intent names

--- skill.py
+++ skill.py
@@ -15,43 +15,42 @@ def homepage():
 @ask.launch
 def start_skill():
 	start = 'Tell me about how you are feeling'
- 	return question (start)
-
+ 	return question(start)
 
 @ask.intent("BadIntent")
 def feeling_good():
 	feeling_bad_resp = 'That sounds tough. What emotions are you feeling right now?'
-	return statement(feeling_bad_resp)
+	return question(feeling_bad_resp)
 
-@ask.intent("IntentFour")
+@ask.intent("NegativeFeelingResponse")
 def find_emotion():
 	emotion_q = "How do you know you are feeling this way?"
-	return statement(emotion_q)
+	return question(emotion_q)
 
-@ask.intent("IntentFive")
+@ask.intent("FeelingExplanationResponse")
 def sympathize():
 	symp = 'Wow, that seems like a lot to deal with and I can imagine very difficult. Could you imagine things being different?'
-	return statement(symp)
+	return question(symp)
 
-@ask.intent("IntentSix")
+@ask.intent("PotentialActionResponse")
 def goal_set():
 	goal = 'That is great! What is a specific goal you can make that can imporve your situation?'
-	return statement(goal)
+	return question(goal)
 
-@ask.intent("IntentSeven")
+@ask.intent("GoalResponse")
 def goal_set_enviro():
 	enviro = 'Identifying a goal is the first step. Next is knowing when you have achieved this goal. How do you know when your goals have been met? What will this look like?'
-	return statement(enviro)
+	return question(enviro)
 
-@ask.intent("IntentEight")
+@ask.intent("QualifyGoalResponse")
 def achievable():
 	achieve = 'Do you think this is an achievable and realistic goal?'
-	return statement(achieve)
+	return question(achieve)
 
-@ask.intent("IntentNine")
+@ask.intent("GoalRealisticAnswer")
 def goal_time():
 	time = 'Great! Now that you have a specific, measurable and realistic goal, what is a realistic time frame to complete your goal?'
-	return statement(time)
+	return question(time)
 
 
 if __name__ == "__main__":
